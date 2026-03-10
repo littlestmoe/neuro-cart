f2379783c8fc5e337523a53ec158627fe633107d
Littlest Moe <littlestmoed@gmail.com>
2026-03-10 13:13:52 +0200

    chore(seller): scaffold basic environment definition for products.ts
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ apps/seller/data/products.ts
@@ -0,0 +1,7 @@
+export const CATEGORIES = [
+  { id: "electronics", name: "Electronics" },
+  { id: "fashion", name: "Fashion" },
+  { id: "home", name: "Home & Living" },
+  { id: "sports", name: "Sports" },
+  { id: "food", name: "Food & Beverages" },
+];
