0a9f70a5e6813464131ae9ed3ddee11878a883f2
Littlest Moe <littlestmoed@gmail.com>
2026-03-11 18:16:07 +0200

    build(bindings): auto-generate TypeScript definitions for add_to_wishlist_reducer
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ packages/shared/src/bindings/cart-server/add_to_wishlist_reducer.ts
@@ -0,0 +1,14 @@
+import {
+  TypeBuilder as __TypeBuilder,
+  t as __t,
+  type AlgebraicTypeType as __AlgebraicTypeType,
+  type Infer as __Infer,
+} from "spacetimedb";
+
+export default {
+  userId: __t.string(),
+  productId: __t.string(),
+  productName: __t.string(),
+  productImage: __t.string(),
+  unitPrice: __t.f64(),
+};
