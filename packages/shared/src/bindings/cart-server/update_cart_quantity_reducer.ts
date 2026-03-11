72bee0d01ee86ddcca04e315d8119722908d00fc
Littlest Moe <littlestmoed@gmail.com>
2026-03-11 21:53:54 +0200

    build(bindings): auto-generate TypeScript definitions for update_cart_quantity_reducer
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ packages/shared/src/bindings/cart-server/update_cart_quantity_reducer.ts
@@ -0,0 +1,11 @@
+import {
+  TypeBuilder as __TypeBuilder,
+  t as __t,
+  type AlgebraicTypeType as __AlgebraicTypeType,
+  type Infer as __Infer,
+} from "spacetimedb";
+
+export default {
+  id: __t.string(),
+  quantity: __t.i32(),
+};
