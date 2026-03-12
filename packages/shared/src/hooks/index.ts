2c6da7e3d7162f02e8d6496e7758b4f37bced7de
Littlest Moe <littlestmoed@gmail.com>
2026-03-12 19:40:36 +0200

    feat(hooks): create custom React context subscriber in index.ts
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ packages/shared/src/hooks/index.ts
@@ -0,0 +1,9 @@
+"use client";
+export * from "./useProducts";
+export * from "./useCart";
+export * from "./useOrders";
+export * from "./useUser";
+export * from "./useReviews";
+export * from "./usePayments";
+export * from "./useModeration";
+export * from "./useAITools";
