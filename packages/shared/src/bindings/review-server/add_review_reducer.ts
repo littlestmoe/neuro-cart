1e2e4633e8c36aa3859279d6a121c5f1ee7a0515
Littlest Moe <littlestmoed@gmail.com>
2026-03-12 10:48:15 +0200

    build(bindings): auto-generate TypeScript definitions for add_review_reducer
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ packages/shared/src/bindings/review-server/add_review_reducer.ts
@@ -0,0 +1,14 @@
+import {
+  TypeBuilder as __TypeBuilder,
+  t as __t,
+  type AlgebraicTypeType as __AlgebraicTypeType,
+  type Infer as __Infer,
+} from "spacetimedb";
+
+export default {
+  productId: __t.string(),
+  userId: __t.string(),
+  userName: __t.string(),
+  rating: __t.i32(),
+  comment: __t.string(),
+};
