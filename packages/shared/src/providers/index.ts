a87ad7032f8c76b72ef2e6b672db512273f9772e
Littlest Moe <littlestmoed@gmail.com>
2026-03-13 02:07:46 +0200

    chore(shared): scaffold basic environment definition for index.ts
    Signed-off-by: Littlest Moe <littlestmoed@gmail.com>

---
+++ packages/shared/src/providers/index.ts
@@ -0,0 +1,31 @@
+export {
+  ProductDbProvider,
+  useProductDb,
+  useProductDbConnection,
+} from "./ProductDbProvider";
+export {
+  CartDbProvider,
+  useCartDb,
+  useCartDbConnection,
+} from "./CartDbProvider";
+export {
+  OrderDbProvider,
+  useOrderDb,
+  useOrderDbConnection,
+} from "./OrderDbProvider";
+export {
+  UserDbProvider,
+  useUserDb,
+  useUserDbConnection,
+} from "./UserDbProvider";
+export {
+  ReviewDbProvider,
+  useReviewDb,
+  useReviewDbConnection,
+} from "./ReviewDbProvider";
+export {
+  PaymentDbProvider,
+  usePaymentDb,
+  usePaymentDbConnection,
+} from "./PaymentDbProvider";
+export { ThemeProvider } from "./ThemeProvider";
